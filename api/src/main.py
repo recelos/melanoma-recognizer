0ec7786541e24ca384c2444c9f00e6ae9c3787d9
recelos <kubagr2000@gmail.com>
2025-01-04 17:30:08 +0100

    Fix API after reorganization

--- api/src/main.py
+++ api/src/main.py
@@ -3,7 +3,7 @@ from PIL import Image
 from fastapi import FastAPI, UploadFile, HTTPException
 import torch
 import torchvision.transforms as transforms
-from model.cnn.CNN import CNN
+from models.cnn.CNN import CNN
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -11,7 +11,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-MODEL_PATH = 'model/cnn/cnn.pt'
+MODEL_PATH = 'models/cnn/cnn.pt'
 
 model = CNN()
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
