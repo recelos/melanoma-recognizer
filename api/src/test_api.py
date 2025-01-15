f69bcb93bb2eceac42644fc82f51e629635b0449
recelos <kubagr2000@gmail.com>
2025-01-15 19:00:17 +0100

    Add benign and malignant tests

--- api/src/test_api.py
+++ api/src/test_api.py
@@ -1,4 +1,5 @@
 import io
+import PIL.Image as Image
 from fastapi.testclient import TestClient
 from main import app
 
@@ -14,3 +15,35 @@ def test_invalid_file_type_should_return_400():
     # Assert
     assert response.status_code == 400
     assert response.json() == {"detail": "Image must be of jpeg type"}
+
+def test_upload_malignant_should_return_malignant():
+    # Arrange
+    filepath = "../../data/train/malignant/melanoma_5000.jpg"
+
+    # Act
+    with open (filepath, 'rb') as image:
+        response = client.post(
+        "/image",
+        files={"file": ("test_image.jpg", image, "image/jpeg")}
+        )
+
+    # Assert
+    assert response.status_code == 200
+    assert "result" in response.json()
+    assert response.json()["result"] == "malignant"
+
+def test_upload_benign_should_return_benign():
+    # Arrange
+    filepath = "../../data/train/benign/melanoma_0.jpg"
+
+    # Act
+    with open (filepath, 'rb') as image:
+        response = client.post(
+        "/image",
+        files={"file": ("test_image.jpg", image, "image/jpeg")}
+        )
+
+    # Assert
+    assert response.status_code == 200
+    assert "result" in response.json()
+    assert response.json()["result"] == "benign"
