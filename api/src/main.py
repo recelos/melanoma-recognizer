be37635df0cbb37c349570d409cc5e690d5e71aa
recelos <kubagr2000@gmail.com>
2025-05-31 16:14:39 +0200

    Fix issues with new version of pytorch

--- api/src/main.py
+++ api/src/main.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, 'cnn.pt')
 
 model = CNN()
-model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
+model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu'), weights_only=False))
 model.eval()
 
 app = FastAPI()
